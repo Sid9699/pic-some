821b3c45cd38abd280069cb17fe0ece938fa39cb
arju15 <arju.nigam@rcggs.com>
2021-09-21 13:22:38 +0530

    log out implementation

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
 
   return <Switch>
     <Route exact path="/">
-      <NavBar></NavBar>
+      <NavBar />
       <MainContent></MainContent>
     </Route>
     <Route path="/login">
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
-import { AppBar, Toolbar as MuitoolBar, Typography, withStyles } from "@material-ui/core";
+import { AppBar, Toolbar as MuitoolBar, Typography, withStyles, Box } from "@material-ui/core";
 import React from "react";
+import { auth } from '../../config';
 
 const Toolbar = withStyles({
     root: {
@@ -7,11 +8,18 @@ const Toolbar = withStyles({
     }
 })(MuitoolBar)
 
-export const NavBar: React.FC = (props) => {
+export const NavBar: React.FC = () => {
+
+    const logOut = async () => {
+        await auth.signOut();
+    }
+
     return <AppBar data-testid="ps-navbar">
         <Toolbar>
             <Typography variant="h5">Pic Some</Typography>
-            {props.children}
+            <Box onClick={logOut}>
+                <Typography variant="body1">Log Out</Typography>
+            </Box>
         </Toolbar>
     </AppBar>;
 }
